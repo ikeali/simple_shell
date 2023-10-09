6ec9ad21d0ba063cad314fcaac8238113c8362da
ikeali <ikechukwualisigwe01@gmail.com>
2023-10-09 05:58:50 -0700

    add.c

--- add.c
+++ add.c
@@ -2,25 +2,29 @@
 #include <stdio.h>
 
 /**
- * _intadd - Adds two integers
- * @int1: the first integer
- * @int2: the second integer
- * Return: The sum of int1 and int2
+ * intadd - Adds two integers.
+ * @int1: the first integer.
+ * @int2: the second integer.
+ *
+ * Return: Sum of int1 and int2.
  */
-	int _intadd(int int1, int int2);
+	int intadd(int int1, int int2)
+	{
+	return (int1 + int2);
+	}
 
-	int(void)
+/**
+ * main - Entry point of the program.
+ *
+ * Return: 0 on success.
+ */
+	int main(void)
 	{
 	int num1 = 10;
 	int num2 = 20;
-	int sum = _intadd(num1, num2);
+	int sum = intadd(num1, num2);
 
 	printf("Sum: %d\n", sum);
 
 	return (0);
-	}
-
-	int _intadd(int int1, int int2)
-	{
-	return (int1 + int2);
 	}
